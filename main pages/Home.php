c0274f996d9142b3794933eaf60044664ba465e4
Blekke2801 <super.nuele01@gmail.com>
2021-05-20 00:22:32 +0200

    utf.8

--- main pages/Home.php
+++ main pages/Home.php
@@ -12,6 +12,7 @@ if (isset($_POST["user"])) {
 ?>
 
 <head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ComVid</title>
