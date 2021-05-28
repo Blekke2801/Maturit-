435fd24165f8a914adfc24e407b93926b96c63ca
Blekke2801 <super.nuele01@gmail.com>
2021-05-28 13:57:45 +0200

    try

--- main pages/lista.php
+++ main pages/lista.php
@@ -1,10 +1,5 @@
 <?  
 require "../utility/Functions.php";
-$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-
-if (strpos($url, 'Home.php') === false) {
-    header("Location:Home.php");
-}
 sec_session_start();
 if(isset($_POST["list"]) && isset($_POST["film"])){
     if($_POST["list"] == "true"){
